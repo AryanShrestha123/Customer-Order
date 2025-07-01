e99b55031d632d44d5aa84c94c1e656afb2e95e5
AryanShrestha123 <aryan4845i@gmail.com>
2025-07-01 12:47:37 +0545

    Templete setup and models creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    email = models.EmailField(max_length=220, unique=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=100, null=True, choices=CATEGORY)
+    description = models.TextField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    )
+
+    #customer = 
+    #customer = 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
